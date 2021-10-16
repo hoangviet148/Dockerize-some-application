95ef1177b103f05f16a0c5d9e3a394f6bc6ceb5b
hoang <nnvvhh148@gmail.com>
2021-10-16 06:46:27 +0000

    update

--- Bareos/scripts/install-bareos.sh
+++ Bareos/scripts/install-bareos.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 cd /app/bareos-deb
 
-dpkg -i libfastlz_0.1-14.2_amd64.deb
-dpkg -i bareos-common_18.2.5-144.1_amd64.deb
-dpkg -i bareos-database-common_18.2.5-144.1_amd64.deb
-dpkg -i bareos-database-mysql_18.2.5-144.1_amd64.deb
-dpkg -i bareos-database-tools_18.2.5-144.1_amd64.deb
-dpkg -i bareos-director_18.2.5-144.1_amd64.deb
-dpkg -i bareos-filedaemon_18.2.5-144.1_amd64.deb
-dpkg -i bareos-storage_18.2.5-144.1_amd64.deb
-dpkg -i bareos-bconsole_18.2.5-144.1_amd64.deb 
-dpkg -i bareos-client_18.2.5-144.1_amd64.deb
-dpkg -i bareos_18.2.5-144.1_amd64.deb
+# dpkg -i libfastlz_0.1-14.2_amd64.deb
+# dpkg -i bareos-common_18.2.5-144.1_amd64.deb
+# dpkg -i bareos-database-common_18.2.5-144.1_amd64.deb
+# dpkg -i bareos-database-mysql_18.2.5-144.1_amd64.deb
+# dpkg -i bareos-database-tools_18.2.5-144.1_amd64.deb
+# dpkg -i bareos-director_18.2.5-144.1_amd64.deb
+# dpkg -i bareos-filedaemon_18.2.5-144.1_amd64.deb
+# dpkg -i bareos-storage_18.2.5-144.1_amd64.deb
+# dpkg -i bareos-bconsole_18.2.5-144.1_amd64.deb 
+# dpkg -i bareos-client_18.2.5-144.1_amd64.deb
+# dpkg -i bareos_18.2.5-144.1_amd64.deb
+# dpkg -i --force-all *.deb
